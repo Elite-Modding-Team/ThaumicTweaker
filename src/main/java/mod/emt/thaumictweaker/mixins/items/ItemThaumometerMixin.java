95b2935cb76fd2c7575ea0e2dfe221df96bbd93c
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2025-09-05 12:31:36 -0700

    fix remap issues

--- src/main/java/mod/emt/thaumictweaker/mixins/items/ItemThaumometerMixin.java
+++ src/main/java/mod/emt/thaumictweaker/mixins/items/ItemThaumometerMixin.java
@@ -13,6 +13,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -32,24 +33,24 @@ import thaumcraft.common.lib.utils.EntityUtils;
 @Mixin(value = ItemThaumometer.class, remap = false)
 public class ItemThaumometerMixin extends Item {
 
-    @Shadow(remap = false)
+    @Shadow
     protected RayTraceResult getRayTraceResultFromPlayerWild(World worldIn, EntityPlayer playerIn, boolean useLiquids) {
         return null;
     }
 
-    @Shadow(remap = false)
+    @Shadow
     private void updateAura(ItemStack stack, World world, EntityPlayerMP player) {
     }
 
-    @Shadow(remap = false)
+    @Shadow
     public void doScan(World worldIn, EntityPlayer playerIn) {
     }
 
-    @Shadow(remap = false)
+    @Shadow
     private void drawFX(final World worldIn, final EntityPlayer playerIn) {
     }
 
-    @Inject(method = "onItemRightClick", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "onItemRightClick", at = @At("HEAD"), cancellable = true, remap = true)
     public void onItemRightClickInject(World world, EntityPlayer p, EnumHand hand, CallbackInfoReturnable<ActionResult<ItemStack>> cir) {
         ItemStack stack = p.getHeldItem(hand);
 
@@ -69,37 +70,37 @@ public class ItemThaumometerMixin extends Item {
     }
 
     @Override
-    public void onUsingTick(ItemStack stack, EntityLivingBase p, int count) {
-        if (!(p instanceof EntityPlayer) || !ConfigTweaksTT.misc_tweaks.legacyThaumometerScanning) return;
-        if (p.world.isRemote) {
+    public void onUsingTick(@NotNull ItemStack stack, @NotNull EntityLivingBase entityLiving, int count) {
+        if (!(entityLiving instanceof EntityPlayer) || !ConfigTweaksTT.misc_tweaks.legacyThaumometerScanning) return;
+        if (entityLiving.world.isRemote) {
             if (count <= 1) {
-                p.stopActiveHand();
-                p.world.playSound(p.posX, p.posY, p.posZ, SoundsTC.scan, SoundCategory.PLAYERS, 1.0F, 1.0F, false);
+                entityLiving.stopActiveHand();
+                entityLiving.world.playSound(entityLiving.posX, entityLiving.posY, entityLiving.posZ, SoundsTC.scan, SoundCategory.PLAYERS, 1.0F, 1.0F, false);
             }
 
             if (count % 2 == 0) {
-                this.drawFX(p.world, (EntityPlayer) p);
-                p.world.playSound(p.posX, p.posY, p.posZ, SoundsTC.ticks, SoundCategory.PLAYERS, 0.2F, 0.45F + p.world.rand.nextFloat() * 0.1F, false);
+                this.drawFX(entityLiving.world, (EntityPlayer) entityLiving);
+                entityLiving.world.playSound(entityLiving.posX, entityLiving.posY, entityLiving.posZ, SoundsTC.ticks, SoundCategory.PLAYERS, 0.2F, 0.45F + entityLiving.world.rand.nextFloat() * 0.1F, false);
             }
         } else {
             if (count <= 1) {
-                this.doScan(p.world, (EntityPlayer) p);
-                if (!p.world.isRemote) ((EntityPlayer) p).getCooldownTracker().setCooldown(this, 5);
+                this.doScan(entityLiving.world, (EntityPlayer) entityLiving);
+                if (!entityLiving.world.isRemote) ((EntityPlayer) entityLiving).getCooldownTracker().setCooldown(this, 5);
             }
         }
     }
 
     @Override
-    public int getMaxItemUseDuration(ItemStack itemstack) {
+    public int getMaxItemUseDuration(@NotNull ItemStack itemstack) {
         return 20;
     }
 
     @Override
-    public EnumAction getItemUseAction(ItemStack stack) {
+    public @NotNull EnumAction getItemUseAction(@NotNull ItemStack stack) {
         return EnumAction.NONE;
     }
 
-    @Inject(method = "onUpdate", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "onUpdate", at = @At("HEAD"), cancellable = true, remap = true)
     public void onUpdateInject(ItemStack stack, World world, Entity entity, int itemSlot, boolean isSelected, CallbackInfo ci) {
         if (isSelected && !world.isRemote && entity.ticksExisted % 20 == 0 && entity instanceof EntityPlayerMP) {
             this.updateAura(stack, world, (EntityPlayerMP) entity);
