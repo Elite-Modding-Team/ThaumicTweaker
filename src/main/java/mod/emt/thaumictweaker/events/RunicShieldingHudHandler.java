faed4bde1350e02c6b3c55c9d032e5771030c528
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2025-09-15 03:54:22 -0700

    fix runic shielding rendering

--- src/main/java/mod/emt/thaumictweaker/events/RunicShieldingHudHandler.java
+++ src/main/java/mod/emt/thaumictweaker/events/RunicShieldingHudHandler.java
@@ -4,15 +4,16 @@ import baubles.api.BaublesApi;
 import baubles.api.cap.IBaublesItemHandler;
 import mod.emt.thaumictweaker.ThaumicTweaker;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
-import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.opengl.GL11;
@@ -24,33 +25,31 @@ import thaumcraft.common.lib.events.PlayerEvents;
 @Mod.EventBusSubscriber(modid = ThaumicTweaker.MOD_ID)
 public class RunicShieldingHudHandler {
     @SubscribeEvent
-    public void renderRunicOnTick(TickEvent.RenderTickEvent event) {
-        if (event.phase == TickEvent.Phase.START) {
-            UtilsFX.sysPartialTicks = event.renderTickTime;
-        } else {
-            Minecraft mc = FMLClientHandler.instance().getClient();
+    public void RenderRunicShielding(RenderGameOverlayEvent.Post event) {
+        Minecraft mc = Minecraft.getMinecraft();
+        ScaledResolution scaledResolution = new ScaledResolution(mc);
+        EntityPlayerSP player = mc.player;
 
-            if (Minecraft.getMinecraft().getRenderViewEntity() instanceof EntityPlayer) {
-                EntityPlayer player = (EntityPlayer) Minecraft.getMinecraft().getRenderViewEntity();
-                long time = System.currentTimeMillis();
+        if(player == null) {
+            return;
+        }
 
-                if (player != null && mc.inGameHasFocus && !player.isCreative() && Minecraft.isGuiEnabled() && this.getTotalRunicArmor(player) > 0 && player.getAbsorptionAmount() > 0) {
-                    renderRunicArmorBar(mc, event.renderTickTime, player, time);
-                }
+        if(!mc.gameSettings.hideGUI && mc.playerController.gameIsSurvivalOrAdventure()) {
+            if(event.getType() == RenderGameOverlayEvent.ElementType.HEALTH) {
+                renderRunicArmorBar(mc, scaledResolution, player);
             }
         }
     }
 
-    private void renderRunicArmorBar(Minecraft mc, float partialTicks, EntityPlayer player, long time) {
+    private void renderRunicArmorBar(Minecraft mc, ScaledResolution scaledResolution, EntityPlayer player) {
         float total = this.getTotalRunicArmor(player);
         float current = player.getAbsorptionAmount();
-        ScaledResolution sr = new ScaledResolution(Minecraft.getMinecraft());
 
         if(total > 0) {
             GlStateManager.pushMatrix();
             GlStateManager.matrixMode(GL11.GL_PROJECTION);
             GlStateManager.loadIdentity();
-            GlStateManager.ortho(0.0D, sr.getScaledWidth_double(), sr.getScaledHeight_double(), 0.0D, 1000.0D, 3000.0D);
+            GlStateManager.ortho(0.0D, scaledResolution.getScaledWidth_double(), scaledResolution.getScaledHeight_double(), 0.0D, 1000.0D, 3000.0D);
             GlStateManager.matrixMode(GL11.GL_MODELVIEW);
             GlStateManager.loadIdentity();
             GlStateManager.translate(0.0F, 0.0F, -2000.0F);
@@ -60,8 +59,8 @@ public class RunicShieldingHudHandler {
             GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
             GlStateManager.disableAlpha();
 
-            int k = sr.getScaledWidth();
-            int l = sr.getScaledHeight();
+            int k = scaledResolution.getScaledWidth();
+            int l = scaledResolution.getScaledHeight();
 
             GlStateManager.translate((float) (k / 2 - 91), (float) (l - 39), 0.0F);
             mc.renderEngine.bindTexture(ParticleEngine.particleTexture);
@@ -88,6 +87,7 @@ public class RunicShieldingHudHandler {
             GlStateManager.color(1.0f, 1.0f, 1.0f, 1.0f);
             GlStateManager.popMatrix();
         }
+        mc.renderEngine.bindTexture(Gui.ICONS);
     }
 
     private int getTotalRunicArmor(EntityPlayer player) {
