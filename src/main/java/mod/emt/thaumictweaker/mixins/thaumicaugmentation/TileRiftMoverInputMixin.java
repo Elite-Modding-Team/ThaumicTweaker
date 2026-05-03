76a221020004b616f0e45aaf4663e5da0016584d
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-05-02 20:29:46 -0700

    fix rift persistence

--- src/main/java/mod/emt/thaumictweaker/mixins/thaumicaugmentation/TileRiftMoverInputMixin.java
+++ src/main/java/mod/emt/thaumictweaker/mixins/thaumicaugmentation/TileRiftMoverInputMixin.java
@@ -2,13 +2,18 @@ package mod.emt.thaumictweaker.mixins.thaumicaugmentation;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import thaumcraft.common.entities.EntityFluxRift;
 import thecodex6824.thaumicaugmentation.common.tile.TileRiftMoverInput;
 
 @Mixin(value = TileRiftMoverInput.class, remap = false)
 public class TileRiftMoverInputMixin {
+    @Shadow protected EntityFluxRift rift;
+
     @Redirect(
             method = "update",
             at = @At(
@@ -33,5 +38,21 @@ public class TileRiftMoverInputMixin {
     )
     private int riftCollapseFixGetSizeMixin(int original) {
         return original <= 2 ? 0 : original;
+    }
+
+    @Inject(
+            method = "update",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/tileentity/TileEntity;getCapability(Lnet/minecraftforge/common/capabilities/Capability;Lnet/minecraft/util/EnumFacing;)Ljava/lang/Object;",
+                    shift = At.Shift.AFTER,
+                    remap = false
+            ),
+            remap = true
+    )
+    private void riftCollapseFixDestroyRiftMixin(CallbackInfo ci) {
+        if(this.rift != null) {
+            this.rift.setDead();
+        }
     }
 }
